9429d67b56c0784c38483b023bdc9c73a8640a57
deep-amn <kauramandeep43636@gmail.com>
2025-10-02 22:31:02 -0500

    Refactor App component to implement routing with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // src/App.tsx
 import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom'; // Import routing tools
 import ThoughtList from './components/thought-list/ThoughtList';
 import PostThoughts from './components/post-thoughts/PostThoughts';
 import ThoughtsFeed from './components/thoughts-feed/ThoughtsFeed';
@@ -19,12 +20,22 @@ const App: React.FC = () => {
       <header className="app-header">
         <h1>ThoughtShare</h1>
         <p>Share your thoughts with the world</p>
+
+        {/* Navigation links */}
+        <nav>
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/post">Post</Link> |{" "}
+          <Link to="/feed">Feed</Link>
+        </nav>
       </header>
 
+      {/*  Define the routes */}
       <main className="app-main">
-        <ThoughtList />
-        <PostThoughts /> 
-        <ThoughtsFeed />
+        <Routes>
+          <Route path="/" element={<ThoughtList />} />
+          <Route path="/post" element={<PostThoughts />} />
+          <Route path="/feed" element={<ThoughtsFeed />} />
+        </Routes>
       </main>
 
       <Footer 
