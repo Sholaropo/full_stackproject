ae2febd09ddfeb7a9259023084b58509f4098524
deep-amn <kauramandeep43636@gmail.com>
2025-10-02 21:51:12 -0500

    Add react-router-dom for routing and wrap App component with BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+// src/main.tsx
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";  // import BrowserRouter
 import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>   
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
