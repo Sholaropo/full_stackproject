56472d6b3f402dee05de6520128f49ca22d87cfa
vbhangu <vbhangu@rrc.ca>
2025-10-04 11:52:09 -0500

    updated the App.tsx file to include the Navigation component in the header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 // src/App.tsx
 import React from "react";
 import ThoughtList from './components/thought-list/ThoughtList';
-import { Routes, Route, Link } from 'react-router-dom'; // Import routing tools
+import { Routes, Route } from 'react-router-dom'; // Import routing tools
 import PostThoughts from './components/post-thoughts/PostThoughts';
 import ThoughtsFeed from './components/thoughts-feed/ThoughtsFeed';
 import Footer from './components/footer/Footer';
+import Navigation from './components/navigation/Navigation';
 import './App.css';
 
 const App: React.FC = () => {
@@ -20,6 +21,7 @@ const App: React.FC = () => {
       <header className="app-header">
         <h1>ThoughtShare</h1>
         <p>Share your thoughts with the world</p>
+        <Navigation />
       </header>
 
       <Routes>
