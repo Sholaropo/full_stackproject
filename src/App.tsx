1ce0c96980f702d2b267d15d5d899ff9bdecc9d2
vbhangu <vbhangu@rrc.ca>
2025-10-05 12:41:46 -0500

    fix all the errors in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 // src/App.tsx
-
-import React from "react";
 import React, { useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom';
 import ThoughtList from './components/thought-list/ThoughtList';
-import { Routes, Route } from 'react-router-dom'; // Import routing tools
 import PostThoughts from './components/post-thoughts/PostThoughts';
 import ThoughtsFeed from './components/thoughts-feed/ThoughtsFeed';
 import Footer from './components/footer/Footer';
@@ -28,25 +24,9 @@ const App: React.FC = () => {
       <header className="app-header">
         <h1>ThoughtShare</h1>
         <p>Share your thoughts with the world</p>
-
         <Navigation />
       </header>
 
-      <Routes>
-          <Route path="/" element={<ThoughtList />} />
-          <Route path="/post" element={<PostThoughts />} />
-          <Route path="/feed" element={<ThoughtsFeed />} />
-        </Routes>
-
-        {/* Navigation links */}
-        <nav>
-          <Link to="/">Home</Link> |{" "}
-          <Link to="/post">Post</Link> |{" "}
-          <Link to="/feed">Feed</Link>
-        </nav>
-      </header>
-
-      {/*  Define the routes */}
       <main className="app-main">
         <Routes>
           <Route 
