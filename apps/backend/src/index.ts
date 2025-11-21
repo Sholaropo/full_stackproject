134ce7dff4fdf78e3cb4c5bb30093a105e52a1ee
vbhangu <vbhangu@rrc.ca>
2025-11-20 23:52:34 -0600

    Resolving Conflicts.

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -7,7 +7,7 @@ import userRoutes from './routes/userRoutes';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
