d0ebe8283cb3008d33635ddd0d7826c05396f924
deep-amn <156330346+deep-amn@users.noreply.github.com>
2025-12-05 12:48:17 -0600

    Update migration.sql
    we have duplicate users in two folders so i am uncommenting this file

--- apps/backend/prisma/migrations/20251115193124_init/migration.sql
+++ apps/backend/prisma/migrations/20251115193124_init/migration.sql
@@ -1,14 +1,14 @@
--- CreateTable
-CREATE TABLE "users" (
-    "id" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
-    "displayName" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "joinDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+//-- CreateTable
+//CREATE TABLE "users" (
+  //  "id" TEXT NOT NULL,
+ //   "username" TEXT NOT NULL,
+ //   "displayName" TEXT NOT NULL,
+ //   "email" TEXT NOT NULL,
+ //   "joinDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ //   "isVerified" BOOLEAN NOT NULL DEFAULT false,
 
-    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
-);
+ //   CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+//);
 
--- CreateIndex
-CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
+//-- CreateIndex
+//CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
